3811ad8ce149f18be6625be8c1ae98c399f0fd72
kepaxabier <kepa.bengoetxea@ehu.eus>
2020-02-18 14:41:35 +0100

    laguntest genera csv correctamente

--- nlpcubeparserprocess.sh
+++ nlpcubeparserprocess.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 source python3envmetrix/bin/activate
-python3 ./laguntest.py $1 $2 $3
+python3 ./laguntest.py $1 $2 $3 $4
 #python3 ./nlpcubeparser.py textos/english.doc.txt
 #Para salir
 deactivate
 cd /var/www/html/erraztest/DepSVG-master
-perl -I src demo/conll_to_svg.perl --dir /var/www/html/erraztest/tmp < $1.syntax.csv
-cd /var/www/html/erraztest/tmp
+perl -I src demo/conll_to_svg.perl --dir /var/www/html/erraztest/$4 < $1.syntax.csv
+cd /var/www/html/erraztest/$4
 for i in *.svg
 do 
 /usr/bin/rsvg-convert $i -o `echo $i | sed -e 's/svg$/png/'` 
 done
-cp /var/www/html/erraztest/DepSVG-master/img/* .
-#rm $1.out.csv
-#rm $1.png
-#rm $1
+#cuando fin de sesion
+#rm -rf /var/www/html/erraztest/$4
+
 
 
