4c41c689404f13176cd851e2916e9a97616a2154
Andrea <andrea98.gar@gmail.com>
2020-03-02 11:10:00 +0100

    index + css

--- index.php
+++ index.php
@@ -7,70 +7,82 @@ require "header.php"
 <head>
 	<script type="text/javascript">
 		function processing(){
-			document.getElementById('processing_img').style.display = 'block';
-			document.getElementById('submit').style.display = 'none';
-			document.getElementById('clearbutton').style.display = 'none';
+			document.getElementById('processing_img').style = "display:inline-block;align: center; color: #FBFBFB; position: relative; left: 50%;";
+			document.getElementById('buttonSubmit').style.display = 'none';
+			document.getElementById('clearButton').style.display = 'none';
+			document.getElementById('inputtext_area').style.display = 'none';
+			document.getElementById('list').style.display = 'none';
 		}
-		function eraseTextAndLink() {
+
+		function eraseText() {
 			document.getElementById("inputtext_area").value = "";
-			document.getElementById('bratlink').style.display = 'none';
-			document.getElementById('pdflink').style.display = 'none';
 		}
-		function eraseEmaDiv() {
-		document.getElementById('emadiv').style.display = 'none';
-		}
+
 	</script>
 </head>
 
 <body>
+  
+		<form id="form" method="post" onsubmit="processing()">
 
-    <div class="input-text-main">
-        <form id="form" method="post" onsubmit="processing()">
-            <textarea id="inputtext_area" rows="15" cols="100" name="inputtext" wrap="physical">
-                <?php
-                    if (isset($_POST['inputtext'])) {
-                        echo $_POST['inputtext'];
-                    }else{
-                        echo "Milk is a white liquid food produced by the mammary glands of mammals. It is the primary source of nutrition for infant mammals. Early-lactation milk contains colostrum, which carries the mother's antibodies to its young. It contains many other nutrients including protein and lactose. \n";
+			<!-- Nivel + Idioma -->
+			<div class="custom-select" id="list">
+				Level:
+				<select name="difficult" class="select-item">
+					<option value="b">Bajo</option>
+					<option value="m">Medio</option>
+					<option value="a">Alto</option>
+				</select>
+				Language:
+				<select name="language"class="select-item">
+					<option value="english">English</option>
+					<option value="basque">Euskera</option>
+				</select>
+			</div>
+
+			<!-- Text Area Input -->
+			<div class="input-text">
+				<textarea id="inputtext_area" rows="5" cols="100" name="inputtext">
+					<?php 
+					if (isset($_POST['inputtext'])) {
+						echo $_POST['inputtext'];
+					}else{
+						echo "Milk is a white liquid food produced by the mammary glands of mammals. It is the primary source of nutrition for infant mammals. Early-lactation milk contains colostrum, which carries the mother's antibodies to its young. It contains many other nutrients including protein and lactose.";
 					}
-                ?>
-            </textarea>
+					?>
+				</textarea>    
+			</div>
 
-            <br>
-
-            <div id="processing_img" style="display:none;"><img src="processing.gif" alt="" /><br>Processing...</div>
-            <button type="submit" id="submit" value="Submit" onclick="javascript:eraseEmaDiv();">Submit</button>
-            <button type="button" id="clearbutton" value="Clear" onclick="javascript:eraseTextAndLink();">Clear</button>
-			<select name="difficult">
-				<option value="b">Bajo</option>
-				<option value="m">Medio</option>
-				<option value="a">Alto</option>
-			</select>
-			<select name="language">
-				<option value="basque">Euskera</option>
-				<option value="english">English</option>
-			</select>
+			<!-- Procesing Gif -->
+			<div id="processing_img" style="display:none;"><img src="processing.gif" alt="" /><br>Processing...</div>
+		
+			<!-- Submit & Clear Buttons -->			
+			<nav class="boton-box">
+				<ol>
+				<li><button type="submit" id="buttonSubmit" value="Submit">Submit</button></li>
+				<li><button type="button" id="clearButton" value="Clear" onclick="javascript:eraseText();">Clear</button></li>
+				</ol>
+			
+			</nav>
         </form>
-    </div>
 
     <?php
 		session_start();
 		$destDir = "/var/www/html/erraztest/";
 		$binPath = "/var/www/html/erraztest/nlpcubeparserprocess.sh";
-		$fileId = date('Y-m-d_His_');
 		$id = uniqid(true);
 		// Execute this code if the submit button is pressed.
 		if (isset($_POST['inputtext'])) {
 			//Crear carpeta aleatoria
 			mkdir($destDir.$id, 0770);
 			//Crear fichero con el texto introducido dentro de la carpeta aleatorio
-			$textPath = $destDir.$id."/text.txt";
+			$completeTextPath = $destDir.$id."/text.txt";
 
 			// Si lo ha creado vacio
-			if ($textPath) {
+			if ($completeTextPath) {
 				//abrimos
-				$f = fopen ($textPath, "w");
-				chmod($textPath, 0664);
+				$f = fopen ($completeTextPath, "w");
+				chmod($completeTextPath, 0664);
 
 				if ($f){
 					//escibimos el contenido de la caja en el fichero
@@ -78,24 +90,20 @@ require "header.php"
 					fwrite($f,"\n");
 					//cerramos el fichero
 					fclose($f);
-					//obtenemos el nombre del fichero sin path
-					$basefn = basename($textPath);
 					//dos2unix - Convertidor de archivos de texto de formato DOS/Mac a Unix y viceversa
-					exec("/usr/bin/dos2unix ".$textPath);
+					exec("/usr/bin/dos2unix ".$completeTextPath);
 					$comando=$binPath." text.txt ".$_POST['difficult']." ".$_POST['language']." ".$destDir.$id;
-					#echo("<p>".$comando."/</p>");
 					exec($comando, $output, $return);
-                                        #echo("<p>".$return."</p>");
-					$emaPath = $textPath.".out.csv";
+					
+					$emaPath = $completeTextPath.".out.csv";
 					$_SESSION["path"] = $emaPath;
 					$_SESSION["basefn"] = $destDir.$id;
-					$_SESSION["carpeta"]=$id;
+					$_SESSION["carpeta"]= $id;
                 }
 
 			} else {
 				echo("<p>Erroreren bat egon da fitxategi tenporala sortzean.</p>");
 			}
-
 		}
     ?>
 
